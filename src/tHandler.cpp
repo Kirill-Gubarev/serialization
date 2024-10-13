0a77ed4e0e5281e05f34734bd71f0da71b1a095e
Kirill-Gubarev <kirillgubarev5668@mail.ru>
2024-10-13 19:12:39 +0700

    fixed conditional compilation

--- src/tHandler.cpp
+++ src/tHandler.cpp
@@ -6,17 +6,23 @@
 #include <unistd.h>
 
 static std::vector<std::function<void()>> termTasks;
+#if defined (__linux__)
 static std::vector<std::function<void()>> suspTasks;
 static std::vector<std::function<void()>> restTasks;
+#endif
 
 void thd::addTermination(const std::function<void()>& func){
 	termTasks.push_back(func);
 }
 void thd::addSuspension(const std::function<void()>& func){
+#if defined (__linux__)
 	suspTasks.push_back(func);
+#endif
 }
 void thd::addRestoration(const std::function<void()>& func){
+#if defined (__linux__)
 	restTasks.push_back(func);
+#endif
 }
 
 static void execute(const std::vector<std::function<void()>>& tasks){
